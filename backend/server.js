2fb459328120f446c126d619826052e4fe4b7d43
Isha Patel <ishapatel1312@gmail.com>
2026-04-23 21:06:43 +0530

    fix correct frontend url

--- backend/server.js
+++ backend/server.js
@@ -33,9 +33,9 @@ SOCKET.IO INITIALIZATION
   const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173",
-      "https://swiftlogictics-frontend.onrender.com"
-    ],
+  "http://localhost:5173",
+  "https://swiftlogistics-sxee.onrender.com"
+],
     methods: ["GET", "POST"]
   }
 });
@@ -88,11 +88,10 @@ app.use("/api", limiter);
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://swiftlogictics-frontend.onrender.com"
+    "https://swiftlogistics-sxee.onrender.com"
   ],
   credentials: true
 }));
-
 // JSON parser
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
