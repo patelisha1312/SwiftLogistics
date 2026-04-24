3b3cd4ad606695d7a79ff1c948713c848e82bc92
Isha Patel <ishapatel1312@gmail.com>
2026-04-24 20:46:49 +0530

    bugs fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected successfully!');
+
+    console.log('✅ MongoDB connected!');
+    console.log('👉 Database name:', mongoose.connection.name);
+    console.log('👉 Host:', mongoose.connection.host);
+
   } catch (err) {
-    console.error('MongoDB Connection Error:', err.message);
+    console.error('❌ MongoDB Connection Error:', err.message);
     process.exit(1);
   }
 };
